512d818e041f9820a5d804fecb16077ddb386560
Sasmita-MK <sasmitamayilsamy@gmail.com>
2024-02-29 07:22:57 +0530

    commit

--- Backend/src/main/java/com/sample/demo/controller/FlightController.java
+++ Backend/src/main/java/com/sample/demo/controller/FlightController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -24,22 +26,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class FlightController {
     @Autowired
     FlightService flightService;
+
     //Create
     @PostMapping("/insert")
-    public FlightBooking insertNewData(@RequestBody FlightBooking InsertThis)
-    {
+    public FlightBooking insertNewData(@RequestBody FlightBooking InsertThis) {
         return flightService.createFlight(InsertThis);
     }
+
     //read
     @GetMapping("/getAll")
-    public List<FlightBooking> getAllFlightData()
-    {
+    public List<FlightBooking> getAllFlightData() {
         return flightService.getFlightBookings();
     }
+
     //Update
     @PutMapping("/update/{id}")
-    public String UpdateTheFlight(@PathVariable int id,@RequestBody FlightBooking fb)
-    {
+    public String UpdateTheFlight(@PathVariable int id,@RequestBody FlightBooking fb) {
         boolean done = flightService.updateFlightBooking(id, fb);
         if(done)
         {
@@ -48,10 +50,10 @@ public class FlightController {
             return "ID Not found Enter the correct ID to be updated";
         }
     }
+
     //Delete
     @DeleteMapping("/delete/{id}")
-    public String DeleteThisFlight(@PathVariable int id)
-    {
+    public String DeleteThisFlight(@PathVariable int id) {
         boolean done = flightService.deleteFlightBooking(id);
         if(done)
         {
@@ -60,4 +62,22 @@ public class FlightController {
             return "ID Not found Enter the correct ID to be deleted";
         }
     }
+
+    // Sort
+   @GetMapping("/sort/{field}")
+   public List<FlightBooking> sortTheFlight(@PathVariable String field) {
+        return flightService.sortFlightBooking(field);
+   }
+
+   // Paginate
+   @GetMapping("/paginate/{offset}/{pageSize}")
+   public List<FlightBooking> paginateTheFlight(@PathVariable int offset, @PathVariable int pageSize) {
+        return flightService.paginateFlightBooking(offset, pageSize);
+   }  
+   
+   // PaginateAndSort
+   @GetMapping("/paginateandsort/{offset}/{pageSize}/{field}") 
+   public List<FlightBooking> paginateAndSortTheFlight(@PathVariable int offset, @PathVariable int pageSize, @PathVariable String field) {
+        return flightService.paginateAndSortFlightBooking(offset, pageSize, field);
+   }
 }
--- Backend/src/main/java/com/sample/demo/service/FlightService.java
+++ Backend/src/main/java/com/sample/demo/service/FlightService.java
@@ -3,48 +3,69 @@ package com.sample.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.sample.demo.model.FlightBooking;
 import com.sample.demo.repository.FlightRepo;
 
-
 @Service
 public class FlightService {
     @Autowired
     FlightRepo flightRepo;
-    //Create
-    public FlightBooking createFlight(FlightBooking flightBooking)
-    {
+
+    // Create
+    public FlightBooking createFlight(FlightBooking flightBooking) {
         return flightRepo.save(flightBooking);
     }
-    //Read
-    public List<FlightBooking> getFlightBookings()
-    {
+
+    // Read
+    public List<FlightBooking> getFlightBookings() {
         return flightRepo.findAll();
     }
-    //Update
-    public boolean updateFlightBooking(int id,FlightBooking fb)
-    {
+
+    // Update
+    public boolean updateFlightBooking(int id, FlightBooking fb) {
         FlightBooking toBeUpdated = flightRepo.findById(id).orElse(null);
-        if(toBeUpdated == null)
-        {
+        if (toBeUpdated == null) {
             return false;
-        }else{
+        } else {
             flightRepo.save(fb);
             return true;
         }
     }
-    //Delete
-    public boolean deleteFlightBooking(int id)
-    {
+
+    // Delete
+    public boolean deleteFlightBooking(int id) {
         FlightBooking toBeDeleted = flightRepo.findById(id).orElse(null);
-        if(toBeDeleted == null)
-        {
+        if (toBeDeleted == null) {
             return false;
-        }else{
+        } else {
             flightRepo.delete(toBeDeleted);
             return true;
         }
+    }
+
+    // Sorting
+    public List<FlightBooking> sortFlightBooking(String field) {
+        Sort sort = Sort.by(field).ascending();
+        return flightRepo.findAll(sort);
+    }
+
+    // Pagination
+    public List<FlightBooking> paginateFlightBooking(int offset, int pageSize) {
+        PageRequest pageRequest = PageRequest.of(offset, pageSize);
+        Page<FlightBooking> page = flightRepo.findAll(pageRequest);
+        return page.toList();
+    }
+
+    // Pagination and Sorting
+    public List<FlightBooking> paginateAndSortFlightBooking(int offset, int pageSize, String field) {
+        Sort sort = Sort.by(field).ascending();
+        PageRequest pageRequest = PageRequest.of(offset, pageSize, sort);
+        Page<FlightBooking> page = flightRepo.findAll(pageRequest);
+        return page.toList();
     }
 }
